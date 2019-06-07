b0bdae8e531d5c6582355416871973580279b5c5
pantxisto <32096239+pantxisto@users.noreply.github.com>
2019-06-07 12:43:45 +0200

    ngDoCheck añadido
    ngDoCheck añadido

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, AfterViewInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  KeyValueDiffers
+} from "@angular/core";
 import * as Highcharts from "highcharts/highstock";
 declare var require: any;
 require("highcharts/modules/export-data");
@@ -9,13 +14,17 @@ require("highcharts/modules/exporting");
   templateUrl: "./chart.component.html",
   styleUrls: ["./chart.component.css"]
 })
-export class ChartComponent implements OnInit, AfterViewInit {
+export class ChartComponent implements OnInit, AfterViewInit, DoCheck {
   chart: Highcharts.Chart;
   serie1: any;
   serie2: any;
+  _inputPayloads: any[];
+  objDiffer: Object;
 
-  constructor() {
+  constructor(private differs: KeyValueDiffers) {
     this.chart = undefined;
+    this.objDiffer = {};
+    this._inputPayloads = [];
   }
 
   ngAfterViewInit(): void {
@@ -37,30 +46,71 @@ export class ChartComponent implements OnInit, AfterViewInit {
       series: []
     });
 
-    const seriesPart1: any = {
-      name: "serie1",
-      data: [new Date().getTime(), parseFloat(50)]
-    };
-    this.chart.addSeries(seriesPart1, true);
+    // const seriesPart1: any = {
+    //   name: "serie1",
+    //   data: [new Date().getTime(), parseFloat(50)]
+    // };
+    // this.chart.addSeries(seriesPart1, true);
 
-    const seriesPart2: any = {
-      name: "serie2",
-      data: [new Date().getTime(), parseFloat(20)]
-    };
-    this.chart.addSeries(seriesPart2, true);
+    // const seriesPart2: any = {
+    //   name: "serie2",
+    //   data: [new Date().getTime(), parseFloat(20)]
+    // };
+    // this.chart.addSeries(seriesPart2, true);
+    this._inputPayloads.push(
+      {
+        cookieWidget: "1",
+        serieName: "serie1",
+        value: "50",
+        timestamp: new Date().getTime()
+      },
+      {
+        cookieWidget: "2",
+        serieName: "serie2",
+        value: "20",
+        timestamp: new Date().getTime()
+      }
+    );
     this.generateData();
+    //this.generateData();
+  }
+
+  ngDoCheck() {
+    this._inputPayloads.forEach(elt => {
+      if (this.objDiffer[elt.cookieWidget] === undefined) {
+        this.objDiffer[elt.cookieWidget] = this.differs
+          .find(elt)
+          .create<null, null>();
+      } else {
+        const objChanges = this.objDiffer[elt.cookieWidget].diff(elt);
+        if (objChanges !== null) {
+          const serieName = elt.serieName;
+          const serie = this.chart.series.filter(
+            srie => srie.name === serieName
+          )[0];
+          if (serie !== undefined) {
+            serie.addPoint([elt.timestamp, parseFloat(elt.value)], true);
+          } else {
+            const seriesPart: any = {
+              name: serieName,
+              data: [elt.timestamp, parseFloat(elt.value)]
+            };
+            this.chart.addSeries(seriesPart, true);
+          }
+        }
+      }
+    });
   }
 
   generateData() {
-    this.serie1 = this.chart.series.filter(srie => srie.name === "serie1")[0];
-    this.serie2 = this.chart.series.filter(srie => srie.name === "serie2")[0];
-
     setInterval(() => {
       const timestamp = new Date().getTime();
       const value1 = Math.random() * (250 - 150) + 150;
       const value2 = Math.random() * (250 - 150) + 150;
-      this.serie1.addPoint([timestamp, parseFloat(value1.toString())], true);
-      this.serie2.addPoint([timestamp, parseFloat(value2.toString())], true);
+      this._inputPayloads[0].value = value1.toString();
+      this._inputPayloads[0].timestamp = timestamp;
+      this._inputPayloads[1].value = value2.toString();
+      this._inputPayloads[1].timestamp = timestamp;
     }, 500);
   }
 }
