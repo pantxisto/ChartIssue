4b5560028450c321e8675f099272adb8c3594351
pantxisto <32096239+pantxisto@users.noreply.github.com>
2019-06-07 10:35:21 +0200

    Added 1 serie
    Added 1 serie

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,26 +1,25 @@
 import { Component, OnInit } from "@angular/core";
-import * as Highcharts from 'highcharts/highstock';
+import * as Highcharts from "highcharts/highstock";
 declare var require: any;
-require('highcharts/modules/export-data');
-require('highcharts/modules/exporting');
+require("highcharts/modules/export-data");
+require("highcharts/modules/exporting");
 
 @Component({
   selector: "app-chart",
   templateUrl: "./chart.component.html",
   styleUrls: ["./chart.component.css"]
 })
-export class ChartComponent implements OnInit{
-  
+export class ChartComponent implements OnInit {
   chart: Highcharts.Chart;
-  actualSerie: any;
+  serie1: any;
+  serie2: any;
 
   constructor() {
-      this.chart = undefined;
-    }
+    this.chart = undefined;
+  }
 
   ngOnInit(): void {
-    this.chart = Highcharts.stockChart('container', {
-
+    this.chart = Highcharts.stockChart("container", {
       chart: {
         width: null,
         height: null
@@ -28,31 +27,39 @@ export class ChartComponent implements OnInit{
       rangeSelector: {
         selected: 4
       },
-      yAxis: {
-        
-      },
+      yAxis: {},
       plotOptions: {
         series: {
-          compare: 'percent',
+          compare: "percent",
           showInNavigator: true
         }
       },
-      series: [],
+      series: []
     });
 
-    const seriesPart: any = {
-      name: 'serie1',
+    const seriesPart1: any = {
+      name: "serie1",
       data: [new Date().getTime(), parseFloat(50)]
     };
-    this.chart.addSeries(seriesPart, true);  }
+    this.chart.addSeries(seriesPart1, true);
+
+    const seriesPart2: any = {
+      name: "serie2",
+      data: [new Date().getTime(), parseFloat(20)]
+    };
+    this.chart.addSeries(seriesPart2, true);
+  }
 
   generateData() {
-    this.actualSerie = this.chart.series.filter(srie => srie.name === 'serie1')[0];
+    this.serie1 = this.chart.series.filter(srie => srie.name === "serie1")[0];
+    this.serie2 = this.chart.series.filter(srie => srie.name === "serie2")[0];
+
     setInterval(() => {
       const timestamp = new Date().getTime();
-      const value = Math.floor(Math.random() * 250) + 150 
-      this.actualSerie.addPoint([timestamp, parseFloat(value.toString())], true);
+      const value1 = Math.floor(Math.random() * 250) + 150;
+      const value2 = Math.floor(Math.random() * 250) + 150;
+      this.serie1.addPoint([timestamp, parseFloat(value1.toString())], true);
+      this.serie2.addPoint([timestamp, parseFloat(value2.toString())], true);
     }, 500);
   }
-  
 }
